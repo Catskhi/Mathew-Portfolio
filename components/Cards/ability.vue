bbb3a83e0feb6f78c5d8c52d231339f3bb869078
Catskhi <catskhi.studios@gmail.com>
2023-11-19 19:50:09 -0300

    fix: Fixed CSS of ability element

--- components/Cards/ability.vue
+++ components/Cards/ability.vue
@@ -9,12 +9,12 @@ const props = defineProps<{
 
 <template>
 <div class="text-white flex items-center px-5">
-    <div class="h-full pt-3">
+    <div class="h-full">
         <Icon name="icon-park-solid:graphic-design" class="text-5xl xl:text-7xl" />
     </div>
     <div class="ml-5">
         <p class="font-inter font-bold text-lg xl:text-2xl">{{ title }}</p>
-        <p class="text-[#A8A8A8] my-2 xl:my-5 xl:text-2xl">{{ description }}</p>
+        <p class="text-[#A8A8A8] my-2  xl:my-5 xl:text-xl">{{ description }}</p>
         <a class="text-[#00FFC2] cursor-pointer hover:text-[#0E9776] xl:text-2xl
              transition-all duration-150">
             Learn More
