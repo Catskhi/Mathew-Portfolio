3b348f5ea77647155d2408c9a73219e12900674e
Catskhi <catskhi.studios@gmail.com>
2023-11-21 23:23:43 -0300

    fix: Created classes for ability component

--- components/Cards/ability.vue
+++ components/Cards/ability.vue
@@ -13,12 +13,23 @@ const props = defineProps<{
         <Icon name="icon-park-solid:graphic-design" class="text-5xl xl:text-7xl" />
     </div>
     <div class="ml-5">
-        <p class="font-inter font-bold text-lg xl:text-2xl">{{ title }}</p>
-        <p class="text-[#A8A8A8] my-2  xl:my-5 xl:text-xl">{{ description }}</p>
-        <a class="text-[#00FFC2] cursor-pointer hover:text-[#0E9776] xl:text-2xl
+        <p class="font-inter font-bold title">{{ title }}</p>
+        <p class="text-[#A8A8A8] my-2  xl:my-5 subtitle">{{ description }}</p>
+        <a class="text-[#00FFC2] cursor-pointer hover:text-[#0E9776] subtitle
              transition-all duration-150">
             Learn More
         </a>
     </div>
 </div>
 </template>
+
+<style scoped>
+.title {
+    @apply text-lg xl:text-xl 2xl:text-2xl;
+}
+
+.subtitle {
+    @apply text-base xl:text-lg 2xl:text-xl
+}
+
+</style>
